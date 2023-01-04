40c60786127b407a73a92053d2195db43175c353
max <kabinidi@mail.ru>
2023-01-04 20:21:42 +0300

    ДЗ ООП Полиморфизм

--- src/transport/Truck.java
+++ src/transport/Truck.java
@@ -8,4 +8,22 @@ public class Truck extends Transport {
     }
 
 
+
+    public enum  LoadCapacity {
+        LOAD_CAPACITY_N1(3.5),
+      //  LOAD_CAPACITY_N2[3.5, 12];
+        LOAD_CAPACITY_N3 (12);
+        private double loadCapacity;
+
+
+        public double getLoadCapacity() {
+            return loadCapacity;
+        }
+
+        LoadCapacity(double loadCapacity) {
+            this.loadCapacity = loadCapacity;
+        }
+    }
+
+
 }
