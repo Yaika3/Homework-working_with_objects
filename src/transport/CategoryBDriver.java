69f1e253d815799a7212af58b2d91be466559233
max <kabinidi@mail.ru>
2023-01-08 13:50:51 +0300

    ДЗ ООП Полиморфизм

--- src/transport/CategoryBDriver.java
+++ src/transport/CategoryBDriver.java
@@ -10,4 +10,5 @@ public class CategoryBDriver <B extends Car & Competing> {
 
     }
 
+
 }
--- src/transport/Driver.java
+++ src/transport/Driver.java
@@ -1,7 +1,15 @@
 package transport;
 
 public class Driver  implements Competing {
+    String fullName;
+    String driversLicense;
+    String experience;
 
+    public Driver(String fullName, String driversLicense, String experience) {
+        this.fullName = fullName;
+        this.driversLicense = driversLicense;
+        this.experience = experience;
+    }
 
     @Override
     public void pitStop() {
